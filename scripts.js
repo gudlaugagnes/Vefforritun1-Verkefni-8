cc37736dfbd96c04807268039501d95d51413b13
Gudlaug Agnes Kristjánsdóttir <gak16@hi.is>
2019-11-05 01:09:10 +0000

    atburðahandlerar á undirklasa items og finish, og delete handler kominn

--- scripts.js
+++ scripts.js
@@ -15,20 +15,38 @@ const text = (() => {
     _form.addEventListener('submit', formHandler);
 
     // TODO láta hluti í _items virka
+
+      const text = document.getElementsByClassName('item__text');
+      for(var i=0;i<text.length; i++){
+        text[i].addEventListener('click', edit);
+      }
+
+      const button = document.getElementsByClassName('item__button');
+      for(var i=0;i<button.length; i++){
+        button[i].addEventListener('click', deleteItem);
+      }
+
+
+      const checkbox = document.getElementsByClassName('item__checkbox');
+      for(var i=0;i<checkbox.length; i++){
+        checkbox[i].addEventListener('click', finish);
+      }
+    
+
   }
 
   function formHandler(e) {
-    e.preventDefault();
-
-    console.log('halló heimur');
+    
   }
 
   // event handler fyrir það að klára færslu
   function finish(e) {
+    e.target.parentNode.classList.toggle('item--done');
   }
 
   // event handler fyrir það að breyta færslu
   function edit(e) {
+    
   }
 
   // event handler fyrir það að klára að breyta færslu
@@ -41,10 +59,12 @@ const text = (() => {
 
   // event handler til að eyða færslu
   function deleteItem(e) {
+    e.target.parentNode.remove();
   }
 
   // hjálparfall til að útbúa element
   function el(type, className, clickHandler) {
+    const el = document.createElement(type);
   }
 
   return {
